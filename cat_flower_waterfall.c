68a1be0d157cf9b537715b9d487318a9f9f19f04
ririchayanx <136694148+ririchayanx@users.noreply.github.com>
2023-06-27 05:43:51 +0700

    Fix broken tests

---
+++ cat_flower_waterfall.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+
+/*
+  This program is about healthy habits!
+*/
+
+//Declare the function
+void HealthyHabits();
+
+//Main function
+int main()
+{
+   //Call the HealthyHabits function
+  HealthyHabits();
+  return 0;
+}
+
+//Function which prints healthy habits
+void HealthyHabits(){
+  
+  printf("1. Eat healthy meals and snacks on a regular basis\n"); 
+  printf("2. Get enough sleep\n");
+  printf("3. Brush teeth twice a day\n");
+  printf("4. Exercise regularly\n");
+  printf("5. Don’t smoke\n");
+  printf("6. Drink alcohol in moderation\n");
+  printf("7. Make time for relaxation\n");
+  printf("8. Change pillowcases every week\n");
+  printf("9. Reduce sugar intake\n");
+  printf("10. Get regular check-ups\n");
+  printf("11. Wear sunscreen\n");
+  printf("12. Drink plenty of water\n");
+  printf("13. Eat breakfast\n");
+  printf("14. Reduce salty foods \n");
+  printf("15. Develop a healthy bedtime routine\n");
+  printf("16. Wash your hands regularly\n");
+  printf("17. Visit the dentist twice a year\n");
+  printf("18. Wear a helmet when appropriate\n");
+  printf("19. Take vitamins\n");
+  printf("20. Wear a seatbelt\n");
+  printf("21. Eat plenty of fresh fruits and vegetables\n");
+  printf("22. Avoid processed foods\n");
+  printf("23. Get a regular massage \n");
+  printf("24. Try to limit your caffeine intake\n");
+  printf("25. Cut back on added sugars\n");
+  printf("26. Manage stress\n");
+  printf("27. Get at least 30 minutes of physical activity every day\n");
+  printf("28. Avoid excessive sun exposure\n");
+  printf("29. Avoid secondhand smoke\n");
+  printf("30. Floss daily\n");
+  printf("31. Exercise your brain everyday \n");
+  printf("32. Take the stairs\n");
+  printf("33. Wash your fruit and vegetables\n");
+  printf("34. Avoid processed meats\n");
+  printf("35. Limit fast food intake\n");
+  printf("36. Be aware of your posture\n");
+  printf("37. Avoid sitting for long periods of time\n");
+  printf("38. Consider supplementing with probiotics\n");
+  printf("39. Eat smaller meals more frequently\n");
+  printf("40. Limit fried foods\n");
+  printf("41. Don’t skip meals\n");
+  printf("42. Wear the right shoes\n");
+  printf("43. Maintain good posture\n");
+  printf("44. Develop an emergency health care plan \n");
+  printf("45. Stretch every day\n");
+  printf("46. Take time to relax\n");
+  printf("47. Avoid unsafe areas\n");
+  printf("48. Limit processed carbohydrates\n");
+  printf("49. Get daily sunshine \n");
+  printf("50. Practice gratitude\n");
+  
+}
