5b98387d87f4c32d37e94540242899809b1e7b8f
ririchayanx <136694148+ririchayanx@users.noreply.github.com>
2023-06-27 12:38:35 +0700

    Enhance error handling

---
+++ queen_jungle_dog.go
@@ -0,0 +1,35 @@
+package main 
+
+import "fmt"
+
+func main() {
+	// Eat Healthy 
+	fmt.Println("Eat breakfast every morning. Eating a healthy breakfast can provide you with energy and essential nutrients to start your day off right.") 
+	fmt.Println("Incorporate a variety of healthy foods into your diet, such as fruits, vegetables, whole grains, and lean proteins.")
+	fmt.Println("Avoid eating too much processed or junk food, as these foods are high in calories and low in nutrition.")
+	
+	// Exercise Regularly
+	fmt.Println("Aim to get at least 30 minutes of physical activity each day, such as walking, running, or swimming.")
+	fmt.Println("Find activities that you enjoy, such as basketball or dancing, and make them part of your regular routine.")
+	fmt.Println("If you can’t find the time to exercise every day, try doing small bursts of activity throughout the day.")
+	
+	// Get Enough Sleep 
+	fmt.Println("Getting enough sleep is an important part of staying healthy. Aim to get 7–8 hours of sleep every night.")
+	fmt.Println("Create a sleep routine that you stick to and avoid activities such as watching TV or using your phone before bed. This can help you wind down and fall asleep more easily.")
+	fmt.Println("If you have difficulty sleeping, talk to your doctor about lifestyle changes or medications that can help you sleep soundly.")
+	
+	// Manage Stress
+	fmt.Println("Take time for yourself every day. Practice positive stress management techniques such as yoga, meditation, or deep breathing.")
+	fmt.Println("Organize your time to help you manage your stress levels. Prioritize your tasks and take regular breaks throughout the day to help you stay focused and productive.")
+	fmt.Println("Speak to a mental health professional if your stress levels become unmanageable. They can help you develop strategies to cope with your stress effectively.")
+	
+	// Practice Mindful Eating
+	fmt.Println("Focus on your food when you’re eating, rather than multitasking or watching TV. This can help you become more aware of when you’re full and prevent overeating.")
+	fmt.Println("Slow down when you eat and savor the flavors of your food. This can help you identify which foods you enjoy and be more mindful of how much you eat in one sitting.")
+	fmt.Println("Eat meals without distractions such as your phone or laptop. This can help you be more mindful of what you’re eating and enjoy the experience more.")
+	
+	// Take Care of Your Mental Health
+	fmt.Println("Schedule time to spend with friends and family. Social connections can help boost your mood and make you feel more supported.")
+	fmt.Println("Stay connected with your community. Joining clubs or volunteering for a cause can give you a sense of purpose and help you make meaningful connections.")
+	fmt.Println("Talk to a therapist or counselor if you’re feeling overwhelmed. They can help you explore your thoughts and feelings and develop strategies to manage them more effectively.")
+}
