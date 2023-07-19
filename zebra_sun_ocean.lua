52a58b4da37e065b659eb5920ca66ab8e9805090
ririchayanx <136694148+ririchayanx@users.noreply.github.com>
2023-07-19 18:13:38 +0700

    Update UI responsiveness

---
+++ zebra_sun_ocean.lua
@@ -0,0 +1,34 @@
+-- Healthy Habits File --
+
+-- Declaring the function
+function HealthyHabits()
+
+	-- Eating healthy
+	print("Eating a healthy, balanced diet is essential for maintaining good health.")
+	print("It's important to make sure you're getting the right vitamins, minerals and nutrients that your body needs.")
+	print("Try to avoid processed and sugary foods as much as possible.")
+
+	-- Getting proper sleep
+	print("Getting enough restful sleep is key to a healthy lifestyle. On average, adults should get between 7-9 hours of sleep each night.")
+	print("Establishing a consistent sleep schedule can help you to get into a regular sleeping routine.")
+	print("Avoiding caffeine, alcohol, and electronics before bed can also help you to get a better night’s rest.")
+
+	-- Staying active
+	print("Regular physical activity is important for both physical and mental health.")
+	print("Try to get at least 30 minutes of exercise each day. It can be anything from running to yoga to dancing.")
+	print("Make sure to switch up your workouts to prevent boredom and muscle fatigue.")
+
+	-- Drinking plenty of water
+	print("Drinking plenty of water is essential for hydration and optimal health.")
+	print("Adults should aim to drink around 8-10 glasses of water per day.")
+	print("Keep a reusable water bottle with you throughout the day to help meet your daily hydration goals.")
+
+	-- Maintaining good hygiene
+	print("Good hygiene is important to ensure both physical and mental health.")
+	print("Be sure to shower or bathe every day, brush your teeth twice a day, and wash your hands frequently throughout the day.")
+	print("With regular proper hygiene, you are less likely to become sick or suffer from skin issues.")
+
+end
+
+-- Calling the function
+HealthyHabits()
