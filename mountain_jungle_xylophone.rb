ecbaf50d6562a85254d2a977dec9925daa3bb09f
ririchayanx <136694148+ririchayanx@users.noreply.github.com>
2023-07-20 16:41:43 +0700

    Enhance data encryption

---
+++ mountain_jungle_xylophone.rb
@@ -0,0 +1,41 @@
+#Healthy Habits in Ruby
+
+#1. Get Enough Sleep
+sleep_time = 8
+puts "Get enough sleep. Aim to get at least #{sleep_time} hours of sleep each night."
+
+#2. Eat a Balanced Diet
+foods = ["fruits", "vegetables", "whole grains", "lean proteins"]
+puts "Eat a balanced diet. Include plenty of #{foods.join(', ')} in your diet."
+
+#3. Exercise Regularly
+exercise_frequency = "3-5 times per week"
+puts "Exercise regularly. Aim for #{exercise_frequency} of moderate to vigorous exercise."
+
+#4. Drink Water
+water_frequency = "8 cups per day"
+puts "Drink enough water. Drink at least #{water_frequency} each day."
+
+#5. Get Fresh Air
+outdoor_time = "60 minutes"
+puts "Get some fresh air. Spend at least #{outdoor_time} outdoors each day to get some sunshine and fresh air."
+
+#6. Practice Mindfulness
+mindfulness_frequency = "daily"
+puts "Practice mindfulness. Take a few moments each day to practice mindfulness or meditation." 
+
+#7. Take Time For Yourself
+self_care_time = "30 minutes"
+puts "Make time for yourself. Take at least #{self_care_time} each day to do something that you enjoy, like reading a book or listening to music." 
+
+#8. Connect With Others
+connection_frequency = "weekly"
+puts "Stay connected. Spend at least #{connection_frequency} with friends and family to stay connected and strengthen relationships." 
+
+#9. Get Regular Check-Ups
+checkup_frequency = "yearly"
+puts "Get regular check-ups. Schedule regular checkups with your doctor to monitor your health and catch any potential issues early."
+
+#10. Reduce Stress
+stress_reduction_tips = ["deep breathing", "take a break from the activity", "exercise"]
+puts "Reduce stress. Try using #{stress_reduction_tips.join(', ')} to reduce your stress levels."
