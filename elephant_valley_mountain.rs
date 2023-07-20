b5fcafd5fff1506decbb35ce94c0a57cede7a8e7
ririchayanx <136694148+ririchayanx@users.noreply.github.com>
2023-07-20 20:26:34 +0700

    Improve file uploading mechanism

---
+++ elephant_valley_mountain.rs
@@ -0,0 +1,169 @@
+// Creating healthy habits
+
+fn main() {
+    println!("Improving your health!");
+
+    // Exercise regularly
+    let daily_exercise: String = "Exercise daily for at least 30 minutes a day.";
+    println!("{}", daily_exercise);
+
+    // Eating a Balanced Diet
+    let balanced_diet: String = "Eat a balanced diet with plenty of fruits and vegetables.";
+    println!("{}", balanced_diet);
+
+    // Drink plenty of water
+    let water_intake: String = "Drink eight glasses of water a day.";
+    println!("{}", water_intake);
+
+    // Get Enough Sleep
+    let sleep_amount: String = "Aim for at least seven hours of sleep every night.";
+    println!("{}", sleep_amount);
+
+    // Quit Smoking
+    let quit_smoking: String = "If you smoke, quit as soon as possible.";
+    println!("{}", quit_smoking);
+
+    // Manage Stress
+    let manage_stress: String = "Manage stress by participating in activities like yoga, meditation and mindfulness.";
+    println!("{}", manage_stress);
+
+    // Regular Check-Ups
+    let regular_checkup: String = "Visit your doctor regularly for check-ups.";
+    println!("{}", regular_checkup);
+
+    // Hand Washing
+    let hand_washing: String = "Wash your hands regularly with soap and water.";
+    println!("{}", hand_washing);
+
+    // Set Realistic Goals
+    let realistic_goals: String = "Set realistic and achievable goals to help you reach your health targets.";
+    println!("{}", realistic_goals);
+
+    // Take Breaks
+    let take_breaks: String = "Make sure to take regular breaks throughout the day to keep your energy levels up.";
+    println!("{}", take_breaks);
+
+    // Reduce Screen Time
+    let reduce_screentime: String = "Limit the amount of time you spend in front of screens such as computers and smart phones.";
+    println!("{}", reduce_screentime);
+
+    // Get Outside
+    let get_outside: String = "Get outside and enjoy some fresh air and physical activity.";
+    println!("{}", get_outside);
+
+    // Laugh Often
+    let laugh_often: String = "Laugh often - it's good for the soul!";
+    println!("{}", laugh_often);
+
+    // Practice Mindfulness
+    let practice_mindfulness: String = "Practice mindfulness - be present in the moment and notice your thoughts and feelings without judgement.";
+    println!("{}", practice_mindfulness);
+
+    // Eat Slowly
+    let eat_slowly: String = "Try to eat slowly and savour each bite. Pay attention to how the food tastes and smells.";
+    println!("{}", eat_slowly);
+
+    // Take Time Off
+    let take_time_off: String = "Take time off to do something you enjoy, like going to the movies or a spa day.";
+    println!("{}", take_time_off);
+
+    // Reduce Caffeine Intake
+    let reduce_caffeine: String = "Reduce your caffeine intake - drinking too much can leave you feeling anxious and tired.";
+    println!("{}", reduce_caffeine);
+
+    // Spend Time With Friends
+    let friends_time: String = "Spend time with friends and family - connection is important for your mental and emotional well-being.";
+    println!("{}", friends_time);
+
+    // Eat Breakfast
+    let breakfast: String = "Eat a healthy breakfast - this will help fuel you for the day ahead.";
+    println!("{}", breakfast);
+
+    // Take the Stairs
+    let stairs: String = "Take the stairs instead of the elevator whenever possible.";
+    println!("{}", stairs);
+
+    // Get Regular Massages
+    let massages: String = "Get regular massages - they can help reduce stress and tension in the body.";
+    println!("{}", massages);
+
+    // Stretch Daily
+    let stretch: String = "Make time to stretch daily - this will improve flexibility and help reduce aches and pains.";
+    println!("{}", stretch);
+
+    // Keep Healthy Snacks On Hand
+    let snacks: String = "Keep healthy snacks on hand, like fruits and vegetables, to help you stay energized throughout the day.";
+    println!("{}", snacks);
+
+    // Practice Gratitude
+    let gratitude: String = "Practice gratitude - take a few moments each day to appreciate the things you are thankful for.";
+    println!("{}", gratitude);
+
+    // Make Time for Fun
+    let fun_time: String = "Make time for fun activities and hobbies that bring you joy.";
+    println!("{}", fun_time);
+
+    // Voluneer
+    let volunteer: String = "Volunteer - helping others can give you a sense of purpose and make you feel good about yourself.";
+    println!("{}", volunteer);
+
+    // Practice Deep Breathing
+    let deep_breathing: String = "Practice deep breathing - this can help reduce stress and tension in the body.";
+    println!("{}", deep_breathing);
+
+    // Try New Things
+    let new_things: String = "Be open to trying new things - this can help keep your life interesting and exciting.";
+    println!("{}", new_things);
+
+    // Avoid Processed Foods
+    let processed_foods: String = "Avoid processed foods - eating too much can lead to health problems.";
+    println!("{}", processed_foods);
+
+    // Reduce Alcohol Intake
+    let reduce_alcohol: String = "Reduce your alcohol intake - drinking too much can lead to health problems.";
+    println!("{}", reduce_alcohol);
+
+    // Take Time to Unplug
+    let unplug: String = "Take time to unplug from technology and connect with the world around you.";
+    println!("{}", unplug);
+
+    // Say No When Necessary
+    let say_no: String = "Learn to say no when necessary - this can help you focus on the things that are most important to you.";
+    println!("{}", say_no);
+
+    // Avoid Negative People
+    let avoid_negativity: String = "Avoid negative people or difficult situations - this can help you stay focused and positive.";
+    println!("{}", avoid_negativity);
+
+    // Read
+    let read: String = "Make time to read - this can help keep your mind sharp and can be a great way to relax.";
+    println!("{}", read);
+
+    // Clean Your Home
+    let clean_home: String = "Make time to clean your home - this can help reduce stress and create a more peaceful environment.";
+    println!("{}", clean_home);
+
+    // Eat Seasonal Foods
+    let seasonal_foods: String = "Eat seasonal foods - they are often fresher and more nutritious than out-of-season foods.";
+    println!("{}", seasonal_foods);
+
+    // Become More Organized
+    let organized: String = "Become more organized - this can help reduce stress and give you a sense of control.";
+    println!("{}", organized);
+
+    // Add More Plant-Based Foods
+    let plant_based_foods: String = "Add more plant-based foods to your diet - they are high in nutrients and can be good for your health.";
+    println!("{}", plant_based_foods);
+
+    // Practice Yoga
+    let yoga: String = "Practice yoga - it can help improve balance, strength, and flexibility and can reduce stress.";
+    println!("{}", yoga);
+
+    // Limit Junk Food
+    let limit_junk: String = "Limit your intake of junk food - too much can lead to health problems and weight gain.";
+    println!("{}", limit_junk);
+
+    // Make Time For Yourself
+    let time_for_you: String = "Make time to do something that you enjoy every day - this can help you relax and recharge.";
+    println!("{}", time_for_you);
+}
